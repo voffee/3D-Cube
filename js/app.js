8e2f6b723f5c600f26b4885012ddbf30a9f49f51
Val <60014136+voffee@users.noreply.github.com>
2026-05-14 22:22:13 +0800

    JS partial

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 
-const getCube = document.querySelector(".cube");
+// Variables for cube movement
+const cube = document.querySelector(".cube");
+let x = 0;
+let y = 0;
 
+function keyHandler(e) {
+    const keyPress = e.key;
 
-console.log(getCube);
+    // Guard Clause to check if keypress is valid
+    if(!keyPress.includes("Arrow")) { return }
+
+    switch (keyPress) {
+
+        case "ArrowUp":
+        // cube.style.setProperty('--y', `${y}px`);
+        console.log(keyPress);
+        break;
+
+        default:
+            console.log("Not a valid rotation key.");
+    }
+}
+
+window.addEventListener("keydown", keyHandler);
